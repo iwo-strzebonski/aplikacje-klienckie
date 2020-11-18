a5156da992f8de8eeca8d32888cbac0cece03b36
iwo-strzebonski <iwo.strzebonski@gmail.com>
2020-11-18 21:46:14 +0100

    Utworzenie osobnej gałęzi na Projekt 001 do Stefańczyka

--- stefańczyk/lekcja 006/server01.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-const PORT = 3000;
-
-app.use(express.static('static'))
-
-app.listen(PORT, function() {
-    console.log('Start serwera 01 na porcie ' + PORT)
-})
