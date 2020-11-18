78e6ce9e928a1ec71fdd6bae6769e620360ba2b1
iwo-strzebonski <iwo.strzebonski@gmail.com>
2020-11-18 18:50:47 +0100

    Dodanie logowania do Projektu 001 do Stefańczyka

--- stefańczyk/projekt 001/server.js
+++ stefańczyk/projekt 001/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require("body-parser")
+const fs = require('fs')
 
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 var site
-var access = false
+access = false
 
 user_data = [
     { id: 0, login: 'AAA', passwd: 'PASS1', age: 10, student: 'on', gender: 'M' },
@@ -18,40 +19,42 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/static/index.html'))
-})
-
 app.get('/:site', function(req, res) {
     site = req.params.site
 
     switch (site) {
-        case 'index.html':
-            res.sendFile(path.join(__dirname + '/static/index.html'))
-            break;
-
         case 'register':
         case 'register.html':
+            access = false
             res.sendFile(path.join(__dirname + '/static/pages/register.html'))
-            break;
+            break
 
         case 'login':
         case 'login.html':
+            access = false
             res.sendFile(path.join(__dirname + '/static/pages/login.html'))
-            break;
+            break
 
         case 'admin':
         case 'admin.html':
+            var data = fs.readFileSync(path.join(__dirname + '/static/pages/admin.html'), 'utf8')
             if (access) {
-                res.sendFile(path.join(__dirname + '/static/pages/admin.html'))
-            } else {
-                res.sendFile(path.join(__dirname + '/static/pages/denied.html'))
-            }
-            break;
+                res.send(data
+                    .replace('<h1></h1>','<h1>admin page</h1>')
+                    .replace('<nav></nav>', "<div class='nav'><a href='sort'>sort</a><a href='gender'>gender</a><a href='show'>show</a></div>")
+                    .replace(" style='display: none;'", ''))
+            } else { res.send(data.replace('<h1></h1>','<h1>access denied</h1>')) }
+            break
+
+        case 'logout':
+            access = false
+            res.redirect('/login')
+            res.end()
+            break
 
         default:
             res.send('Taka strona nie istnieje')
-            break;
+            break
     }
 })
 
@@ -69,6 +72,27 @@ app.post('/register', (req, res) => {
         user_data.push(req.body.id)
     } else {
         res.send('User with that login already exists!')
+    }
+
+    res.end()
+})
+
+app.post('/login', (req, res) => {
+    var bool = false
+
+    for (let i = 0; i < user_data.length; i++) {
+        if (req.body.login === user_data[i].login &&
+            req.body.passwd === user_data[i].passwd) {
+                bool = true
+                break
+            } else { bool = false }
+    }
+
+    if (bool) {
+        access = true
+        res.redirect('admin');
+    } else {
+        res.send('Invalid credentials')
     }
 
     res.end()
