f9495240f56af58b4f0da311737b1b76e4fdabbc
Iwo Strzeboński <iwo.strzebonski@gmail.com>
2021-10-06 23:16:25 +0200

    Poprawiona Lekcja 001 do Mendeli

--- Mendela/lekcja 001/main.py
+++ Mendela/lekcja 001/main.py
@@ -1,67 +1,67 @@
-import re
-import json
-import pprint
-import shutil
-import tempfile
-from datetime import datetime
-
+'''
+Simple script used to recursively scrape liks and e-mail addresses
+using urllib and save them to a JSON file
+'''
 
 import urllib.request
+import re
+import json
+import shutil
+import tempfile
+
+from socket import timeout
 from http.client import InvalidURL
 from urllib.error import HTTPError, URLError
 
-open('data.json', 'w', encoding='utf-8').close()
+__author__ = 'Iwo Strzeboński'
+__license__ = 'WTFPL'
+__version__ = '1.0.0'
 
-regexL = 'a .*?href=\"(https?:\/\/[^\#]*?)\"'
-regexM = '^\S+@\S+\.\S+$'
-url = 'https://tl.krakow.pl/'
 
-addrs = []
-recursive = 3
+def get_links(url: str, rec: int) -> None:
+    '''Generates list of 10 links and 10 e-mail adresses from a website
 
-def getLinks(url : str, rec : int) -> list:
-    global addrs
+    Args:
+        url (str): Website's URL address
+        rec (int): Recursive counter
+    '''
 
     links = []
     mails = []
 
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(url, timeout=5) as response:
             with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
                 shutil.copyfileobj(response, tmp_file)
-    except (InvalidURL, HTTPError, URLError, ConnectionResetError):
-        links = []
-        mails = []
+    except (InvalidURL, HTTPError, URLError, ConnectionResetError, timeout):
+        pass
     else:
-        with open(tmp_file.name) as html:
+        with open(tmp_file.name, 'r', encoding='utf-8') as html:
             try:
-                links = re.findall(regexL, html.read())
-                mails = re.findall(regexL, html.read())
+                data = html.read()
+                links = re.findall(REGEX_L, data)[:10]
+                mails = re.findall(REGEX_M, data)[:10]
             except UnicodeDecodeError:
-                links = []
-                mails = []
-            else:
-                links = links[:10]
-                mails = mails[:10]
+                pass
 
-    with open('data.json', 'a', encoding='utf-8') as f:
-        json.dump({'url': url, 'links': links, 'mails': mails}, f)
-        f.write('\n')
+    ADDRS.append({'url': url, 'links': links, 'mails': mails})
 
     if rec > 0:
-        [getLinks(url, rec-1) for url in links]
-    
-    return
+        for link in links:
+            get_links(link, rec-1)
 
+RECURSIVE = 3
+ADDRS = []
+REGEX_L = r'a .*?href=\"(https?:\/\/[^\#]*?)\"'
+REGEX_M = r'([\w.+-]+@[\w-]+\.[\w.-]+)'
+URL = \
+    'https://www.zoho.com/mail/how-to/choose-a-professional-email-address.html'
 
-start = datetime.utcnow().timestamp() * 1000
-getLinks(url, recursive)
-stop = datetime.utcnow().timestamp() * 1000
+if __name__ == '__main__':
+    get_links(URL, RECURSIVE)
 
-with open('data.json', 'r+', encoding='utf-8') as f:
-    json.dump(f.readlines(), f, indent=2)
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(ADDRS, f)
+        f.close()
 
-print(stop - start)
-
-# with open('data.json', 'w', encoding='utf-8') as f:
-#     json.dump(addrs, f, ensure_ascii=False, indent=4)
+    print('\aDone!')
